ab20fee205f2c1f97ce2ab5fbaee24c4bf0ee90e
Maxime Rogissart <maxime@sommesi.com>
2025-02-13 17:42:13 +0100

    removing skipping logic

--- lib/munster/jobs/processing_job.rb
+++ lib/munster/jobs/processing_job.rb
@@ -8,13 +8,7 @@ module Munster
       Rails.error.set_context(munster_handler_module_name: webhook.handler_module_name, **Munster.configuration.error_context)
 
       webhook_details_for_logs = "Munster::ReceivedWebhook#%s (handler: %s)" % [webhook.id, webhook.handler]
-      webhook.with_lock do
-        unless webhook.received?
-          logger.info { "#{webhook_details_for_logs} is being processed in a different job or has been processed already, skipping." }
-          return
-        end
-        webhook.processing!
-      end
+      webhook.processing!
 
       if webhook.handler.valid?(webhook.request)
         logger.info { "#{webhook_details_for_logs} starting to process" }
